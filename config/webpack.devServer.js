b8dd13f0ca1ee3e97d4fb317297386a92a22e549
Krists <kpudzens@gmail.com>
2021-01-09 18:02:55 +0200

    Initial commit

---
+++ config/webpack.devServer.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+    devServer: {
+        index: 'index.html',
+        contentBase: path.join(__dirname, '../dist'),
+        compress: true,
+        host: '0.0.0.0',
+        port: 9001
+    }
+};
