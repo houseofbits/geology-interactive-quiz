8a85ae572c982e2a063025bd5f84a64cf10b0df7
Krists <kpudzens@gmail.com>
2021-01-17 12:04:43 +0200

    d

--- app/js/gui/Services/ObjectDetectionService.js
+++ app/js/gui/Services/ObjectDetectionService.js
@@ -22,67 +22,67 @@ export default class ObjectDetectionService {
         return pointDist;
     }
 
-    // detectObject(objectDef, pointDist) {
-    //     for (let i = 0; i < pointDist.length; i++) {
-    //         //Candidate A
-    //         if (objectDef.matchSegmentLength(0, pointDist[i].distance)) {
-    //             for (let j = 0; j < pointDist.length; j++) {
-    //                 //Candidate B
-    //                 if (i !== j && objectDef.matchSegmentLength(1, pointDist[j].distance)) {
-    //                     for (let k = 0; k < pointDist.length; k++) {
-    //                         if (i !== j && i !== k && j !== k && objectDef.matchSegmentLength(2, pointDist[k].distance)) {
-    //                             if (this.validateIndices(i, j, k, pointDist)) {
-    //                                 return new ObjectDetectionResult(i, j, k, this.calculateWeight(pointDist, i, j, k, objectDef));
-    //                             }
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     return null;
-    // }
-
     detectObject(objectDef, pointDist) {
         for (let i = 0; i < pointDist.length; i++) {
+            //Candidate A
             if (objectDef.matchSegmentLength(0, pointDist[i].distance)) {
-                return new ObjectDetectionResult(pointDist[i].indexA, pointDist[i].indexB, objectDef.calculateWeight(0, pointDist[i].distance));
+                for (let j = 0; j < pointDist.length; j++) {
+                    //Candidate B
+                    if (i !== j && objectDef.matchSegmentLength(1, pointDist[j].distance)) {
+                        for (let k = 0; k < pointDist.length; k++) {
+                            if (i !== j && i !== k && j !== k && objectDef.matchSegmentLength(2, pointDist[k].distance)) {
+                                if (this.validateIndices(i, j, k, pointDist)) {
+                                    return new ObjectDetectionResult(i, j, k, this.calculateWeight(pointDist, i, j, k, objectDef));
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
+        return null;
     }
 
-    // validateIndices(i, j, k, pointDist) {
-    //     let pointIndexes = new Set();
-    //     pointIndexes.add(pointDist[i].indexA);
-    //     pointIndexes.add(pointDist[i].indexB);
-    //     pointIndexes.add(pointDist[j].indexA);
-    //     pointIndexes.add(pointDist[j].indexB);
-    //     pointIndexes.add(pointDist[k].indexA);
-    //     pointIndexes.add(pointDist[k].indexB);
-    //     return pointIndexes.size === 3;
+    // detectObject(objectDef, pointDist) {
+    //     for (let i = 0; i < pointDist.length; i++) {
+    //         if (objectDef.matchSegmentLength(0, pointDist[i].distance)) {
+    //             return new ObjectDetectionResult(pointDist[i].indexA, pointDist[i].indexB, objectDef.calculateWeight(0, pointDist[i].distance));
+    //         }
+    //     }
     // }
 
-    // calculateWeight(actualDI, objectDef) {
-    //     let weight = 0.0;
-    //
-    //     weight += objectDef.calculateWeight(0, pointDist[i].distance);
-    //     weight += objectDef.calculateWeight(1, pointDist[j].distance);
-    //     weight += objectDef.calculateWeight(2, pointDist[k].distance);
-    //
-    //     return weight / 3.0;
-    // }
+    validateIndices(i, j, k, pointDist) {
+        let pointIndexes = new Set();
+        pointIndexes.add(pointDist[i].indexA);
+        pointIndexes.add(pointDist[i].indexB);
+        pointIndexes.add(pointDist[j].indexA);
+        pointIndexes.add(pointDist[j].indexB);
+        pointIndexes.add(pointDist[k].indexA);
+        pointIndexes.add(pointDist[k].indexB);
+        return pointIndexes.size === 3;
+    }
+
+    calculateWeight(pointDist, i, j, k, objectDef) {
+        let weight = 0.0;
+
+        weight += objectDef.calculateWeight(0, pointDist[i].distance);
+        weight += objectDef.calculateWeight(1, pointDist[j].distance);
+        weight += objectDef.calculateWeight(2, pointDist[k].distance);
+
+        return weight / 3.0;
+    }
 
     getDetectedPosition(detectionResult, touchPoints) {
 
-        let x = touchPoints[detectionResult.i].x + touchPoints[detectionResult.j].x;
-        let y = touchPoints[detectionResult.i].y + touchPoints[detectionResult.j].y;
+        let x = touchPoints[detectionResult.i].x + touchPoints[detectionResult.j].x + touchPoints[detectionResult.k].x;
+        let y = touchPoints[detectionResult.i].y + touchPoints[detectionResult.j].y + touchPoints[detectionResult.k].y;
 
-        return new DetectedObjectPosition(x / 2, y / 2);
+        return new DetectedObjectPosition(x / 3, y / 3);
     }
 
     calculateObjectDefinition(touchPoints, error) {
         const distances = this.calculatePointDistances(touchPoints, 0, 10000);
-        if(distances.length === 1) {
+        if(distances.length === 3) {
             const objDef = new ObjectDefinition();
             for (const dist of distances) {
                 objDef.addSegment(dist.distance - error, dist.distance + error)
--- app/js/gui/Stuctures/ObjectDetectionResult.js
+++ app/js/gui/Stuctures/ObjectDetectionResult.js
@@ -1,14 +1,14 @@
 export default class ObjectDetectionResult {
-    // constructor(i,j,k, weight) {
-    //     this.i = i;
-    //     this.j = j;
-    //     this.k = k;
-    //     this.weight = weight;
-    // }
-
-    constructor(i, j, weight) {
+    constructor(i,j,k, weight) {
         this.i = i;
         this.j = j;
+        this.k = k;
         this.weight = weight;
     }
+
+    // constructor(i, j, weight) {
+    //     this.i = i;
+    //     this.j = j;
+    //     this.weight = weight;
+    // }
 }
