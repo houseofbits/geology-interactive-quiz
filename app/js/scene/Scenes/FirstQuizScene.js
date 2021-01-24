870cfb12fb6341cb2bbc2af700344db4d5560d49
Krists <kpudzens@gmail.com>
2021-01-24 20:42:55 +0200

    f

--- app/js/scene/Scenes/FirstQuizScene.js
+++ app/js/scene/Scenes/FirstQuizScene.js
@@ -25,21 +25,21 @@ export default class FirstQuizScene extends BaseScene {
 
     createScene() {
 
-        // // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-        // const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), this.scene);
-        //
-        // // Default intensity is 1. Let's dim the light a small amount
-        // light.intensity = 0.7;
-        //
-        // // Our built-in 'sphere' shape.
-        // const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, this.scene);
-        //
-        // // Move the sphere upward 1/2 its height
-        // sphere.position.y = 1;
-        //
-        // // Our built-in 'ground' shape.
-        // const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, this.scene);
-        //
+        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+        const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), this.scene);
+
+        // Default intensity is 1. Let's dim the light a small amount
+        light.intensity = 0.7;
+
+        // Our built-in 'sphere' shape.
+        const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, this.scene);
+
+        // Move the sphere upward 1/2 its height
+        sphere.position.y = 1;
+
+        // Our built-in 'ground' shape.
+        const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, this.scene);
+
 
 
 
