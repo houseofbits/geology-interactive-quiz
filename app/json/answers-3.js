22b0a94ccf5f66273f2fb96fb9806fe7673c34a1
houseofbits <krists.pudzens@gmail.com>
2022-10-13 08:18:59 +0300

    s

---
+++ app/json/answers-3.js
@@ -0,0 +1,42 @@
+[
+    {
+      id:0
+      "answer": "Delta"
+      a:1
+    },
+    {
+      id:1
+      "answer": "Erozija"
+      a:2
+    },
+    {
+      id:2
+      "answer": "Humīds klimats"
+      a:3
+    },
+    {
+      id:3
+      "answer": "Arīds klimats"
+      a:4
+    },
+    {
+      id:4
+      "answer": "Fizikālā dēdēšana"
+      a:5
+    },
+    {
+      id:5
+      "answer": "Ķīmiskā dēdēšana"
+      a:6
+    },
+    {
+      id:6
+      "answer": "Dēdējumgaroza"
+      a:8
+    },
+    {
+      id:7
+      "answer": "Mālu minerāli"
+      a:7
+    }
+  ]
