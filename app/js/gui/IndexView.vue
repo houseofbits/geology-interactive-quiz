10c04224e7c982ac8b0f1034afff037e6bd0806d
Krists Pudzens <kpudzens@gmail.com>
2026-02-12 19:35:02 +0200

    a

--- app/js/gui/IndexView.vue
+++ app/js/gui/IndexView.vue
@@ -20,22 +20,22 @@
 <!--                                 to="/detector">-->
 <!--                        Detector test-->
 <!--                    </router-link>-->
-                    <router-link class="btn btn-lg btn-primary d-flex align-items-center justify-content-center"
+                    <!-- <router-link class="btn btn-lg btn-primary d-flex align-items-center justify-content-center"
                                  to="/detector2">
                         Detector
-                    </router-link>
+                    </router-link> -->
                     <router-link class="btn btn-lg btn-primary d-flex align-items-center justify-content-center"
                                  to="/io">
                         IO Test
                     </router-link>
-                    <router-link class="btn btn-lg btn-primary d-flex align-items-center justify-content-center"
+                    <!-- <router-link class="btn btn-lg btn-primary d-flex align-items-center justify-content-center"
                                  to="/touch">
                         Multitouch
                     </router-link>
                     <router-link class="btn btn-lg btn-primary d-flex align-items-center justify-content-center"
                                  to="/error">
                         Error regions
-                    </router-link>
+                    </router-link> -->
                 </div>
                 <div class="col-sm bg-dark"></div>
             </div>
